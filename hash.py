e7d5dc7fdb7e3e3793874b7ca8e44bdd3736d174
gameboy-stack <gt173211@gmail.com>
2021-07-04 23:13:48 +0530

    Finished

--- hash.py
+++ hash.py
@@ -2,30 +2,52 @@ import hashlib as hl
 
 def md5hash(text):
     res = hl.md5(text.encode())
-    print("this is md5",res.hexdigest())
+    print("The MD5 Hash value for given text is : {}".format(res.hexdigest()))
 
 def sha256hash(text):
     res = hl.sha256(text.encode())
-    print("this is sha256")
+    print("The SHA256 Hash value for given text is : {}".format(res.hexdigest()))
 
 def sha512hash(text):
     res = hl.sha512(text.encode())
-    print("this is sha512")
+    print("The SHA512 Hash value for given text is : {}".format(res.hexdigest()))
 
 def sha384hash(text):
-    print("sha384")
+    res = hl.sha384(text.encode())
+    print("The SHA384 Hash value for given text is : {}".format(res.hexdigest()))
 
-def sha1hash(text,iter):
-    print("this is sha1")
+def sha1hashnsalting(res,iter,salttext):
 
-inptext = str(input("Enter Text :-"))
-choice = input("""
+    res = ''.join([(lambda a,b: b+salttext if(a%2!=0) else b)(i,t) for i,t in enumerate(res)])
+
+    for i in range(iter):
+        res = hl.sha1(res.encode()).hexdigest()
+
+    print("The Result Hash value is : {}".format(res))
+
+inptext = str(input("Enter Text :- "))
+choice = str(input("""
 1. MD5
 2. Sha256
 3. Sha512
 4. Sha384
 5. Sha1 with sal
+Enter Your Choice :- """))
 
-Enter Your Choice:-""")
-md5hash(inptext)
+if(choice=="1"):
+    md5hash(inptext)
+
+elif(choice=="2"):
+    sha256hash(inptext)
+
+elif(choice=="3"):
+    sha512hash(inptext)
+
+elif(choice=="4"):
+    sha384hash(inptext)
+
+else:
+    salting_text = input("Enter salting text :- ")
+    iterinp = int(input("Enter no of iteration to be performed :- "))
+    sha1hashnsalting(inptext,iterinp,salting_text)
 
